19fdb9c46502543dc5e1e836f34afe93396f4336
Norimasa Nabeta <norimasanabeta@me.com>
2013-02-22 08:27:18 +0900

    basic configuration.

---
+++ SPoT/FlickrFetcher/FlickrAPIKey.h
@@ -0,0 +1,13 @@
+//
+//  FlickrAPIKey.h
+//
+//  Created for Stanford CS193p Winter 2013.
+//  Copyright 2013 Stanford University
+//  All rights reserved.
+//
+//  Get your own key!
+//  No Flickr fetches will work without the API Key!
+//
+
+// #define FlickrAPIKey @""
+#define FlickrAPIKey @"495169d4c351cef1b5fa5d2e664925d0"
